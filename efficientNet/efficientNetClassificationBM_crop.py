9328ce88a58d520bc6ed192a0bfa523bb37bb1e9
DESKTOP-VOMDDOA\Vincent <karta1110257@gmail.com>
2022-03-14 16:13:36 +0800

    upload_ef

---
+++ efficientNet/efficientNetClassificationBM_crop.py
@@ -0,0 +1,313 @@
+# -*- coding: utf-8 -*-
+
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from tqdm import tqdm
+
+
+import matplotlib.pyplot as plt 
+import torch.nn.functional as F 
+import torch 
+import numpy as np 
+
+def show_image(image,label,get_denormalize = True):
+    
+    image = image.permute(1,2,0)
+    mean = torch.FloatTensor([0.485, 0.456, 0.406])
+    std = torch.FloatTensor([0.229, 0.224, 0.225])
+    
+    if get_denormalize == True:
+        image = image*std + mean
+        image = np.clip(image,0,1)
+        plt.imshow(image)
+        plt.title(label)
+        
+    else: 
+        plt.imshow(image)
+        plt.title(label)
+
+def show_grid(image,title = None):
+    
+    image = image.permute(1,2,0)
+    mean = torch.FloatTensor([0.485, 0.456, 0.406])
+    std = torch.FloatTensor([0.229, 0.224, 0.225])
+    
+    image = image*std + mean
+    image = np.clip(image,0,1)
+    
+    plt.figure(figsize=[15, 15])
+    plt.imshow(image)
+    if title != None:
+        plt.title(title)
+
+
+def accuracy(y_pred,y_true):
+    y_pred = F.softmax(y_pred,dim = 1)
+    top_p,top_class = y_pred.topk(1,dim = 1)
+    equals = top_class == y_true.view(*top_class.shape)
+    return torch.mean(equals.type(torch.FloatTensor))
+
+
+def view_classify(image,ps,label):
+    
+    class_name = ['BENIGN', 'MALIGNANT']
+    classes = np.array(class_name)
+
+    ps = ps.cpu().data.numpy().squeeze()
+    
+    image = image.permute(1,2,0)
+    mean = torch.FloatTensor([0.485, 0.456, 0.406])
+    std = torch.FloatTensor([0.229, 0.224, 0.225])
+    
+    
+    image = image*std + mean
+    img = np.clip(image,0,1)
+    
+    fig, (ax1, ax2) = plt.subplots(figsize=(8,12), ncols=2)
+    ax1.imshow(img)
+    ax1.set_title('Ground Truth : {}'.format(class_name[label]))
+    ax1.axis('off')
+    ax2.barh(classes, ps)
+    ax2.set_aspect(0.1)
+    ax2.set_yticks(classes)
+    ax2.set_yticklabels(classes)
+    ax2.set_title('Predicted Class')
+    ax2.set_xlim(0, 1.1)
+
+    plt.tight_layout()
+
+    return None
+
+class CFG:
+
+  epochs = 50                              # No. of epochs for training the model
+  lr = 0.001                              # Learning rate
+  batch_size = 16                         # Batch Size for Dataset
+
+  model_name = 'tf_efficientnet_b4_ns'    # Model name (we are going to import model from timm)
+  img_size = 640                          # Resize all the images to be 224 by 224
+
+  # going to be used for loading dataset
+  #Data_DIR = "chest_xray"
+  #TEST = "test"
+  #TRAIN = "train"
+  #VAL = "val"
+  train_path='/ddsmDataset/classification_BM_DDSM_crop/train'
+  validate_path='/ddsmDataset/classification_BM_DDSM_crop/val'
+  test_path='/ddsmDataset/classification_BM_DDSM_crop/val'
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("On which device we are on:{}".format(device))
+
+from torchvision import transforms as T,datasets
+
+train_transform = T.Compose([
+                             
+                             T.Resize(size=(CFG.img_size,CFG.img_size)), # Resizing the image to be 224 by 224
+                             T.RandomRotation(degrees=(-90,+90)), #Randomly Rotate Images by +/- 20 degrees, Image argumentation for each epoch
+                             T.RandomHorizontalFlip(0.5),
+                             T.RandomVerticalFlip(0.5),
+                             T.ToTensor(), #converting the dimension from (height,weight,channel) to (channel,height,weight) convention of PyTorch
+                             T.Normalize([0.485,0.456,0.406],[0.229,0.224,0.225]) # Normalize by 3 means 3 StD's of the image net, 3 channels
+
+])
+
+validate_transform = T.Compose([
+                             
+                             T.Resize(size=(CFG.img_size,CFG.img_size)), # Resizing the image to be 224 by 224
+                             #T.RandomRotation(degrees=(-20,+20)), #NO need for validation
+                             T.ToTensor(), #converting the dimension from (height,weight,channel) to (channel,height,weight) convention of PyTorch
+                             T.Normalize([0.485,0.456,0.406],[0.229,0.224,0.225]) # Normalize by 3 means 3 StD's of the image net, 3 channels
+
+])
+
+test_transform = T.Compose([
+                             
+                             T.Resize(size=(CFG.img_size,CFG.img_size)), # Resizing the image to be 224 by 224
+                             #T.RandomRotation(degrees=(-20,+20)), #NO need for validation
+                             T.ToTensor(), #converting the dimension from (height,weight,channel) to (channel,height,weight) convention of PyTorch
+                             T.Normalize([0.485,0.456,0.406],[0.229,0.224,0.225]) # Normalize by 3 means 3 StD's of the image net, 3 channels
+
+])
+
+trainset=datasets.ImageFolder(CFG.train_path,transform=train_transform)
+print("Trainset Size:  {}".format(len(trainset)))
+
+validateset=datasets.ImageFolder(CFG.validate_path,transform=validate_transform)
+print("validateset Size:  {}".format(len(validateset)))
+
+testset=datasets.ImageFolder(CFG.test_path,transform=test_transform)
+print("testset Size:  {}".format(len(testset)))
+
+img,label = trainset[20]
+#print(trainset.class_to_idx)
+
+class_name =['BENIGN', 'MALIGNANT']
+#show_image(img,class_name[label])
+
+img,label = trainset[20]
+#print(trainset.class_to_idx)
+
+class_name =['BENIGN', 'MALIGNANT']
+#show_image(img,class_name[label]) 
+
+# randomly rotated
+
+from torch.utils.data import DataLoader
+from torchvision.utils import make_grid
+
+trainloader = DataLoader(trainset,batch_size=CFG.batch_size,shuffle=True)
+print("No. of batches in trainloader:{}".format(len(trainloader))) #Trainset Size:  5216 / batch_size: 16 = 326(No. of batches in trainloader) 
+print("No. of Total examples:{}".format(len(trainloader.dataset)))
+
+validationloader = DataLoader(validateset,batch_size=CFG.batch_size,shuffle=True)
+print("No. of batches in validationloader:{}".format(len(validationloader))) #validationset Size:  16 / batch_size: 16 = 1(No. of batches in validationloader) 
+print("No. of Total examples:{}".format(len(validationloader.dataset)))
+
+testloader = DataLoader(testset,batch_size=CFG.batch_size,shuffle=True)
+print("No. of batches in testloader:{}".format(len(testloader))) #testset Size:  624 / batch_size: 16 = 39(No. of batches in testloader) 
+print("No. of Total examples:{}".format(len(testloader.dataset)))
+
+dataiter = iter(trainloader)
+images,labels = dataiter.next()
+
+out = make_grid(images,nrow=4)
+
+#show_grid(out,title = [class_name[x] for x in labels])
+
+from torch import nn
+import torch.nn.functional as F
+import timm # PyTorch Image Models
+
+model = timm.create_model(CFG.model_name,pretrained=True) #load pretrained model
+
+#model
+
+#let's update the pretarined model:
+for param in model.parameters():
+  param.requires_grad=False
+
+#orginally, it was:
+#(classifier): Linear(in_features=1792, out_features=1000, bias=True)
+
+
+#we are updating it as a 2-class classifier:
+model.classifier = nn.Sequential(
+    nn.Linear(in_features=1792, out_features=2), #1792 is the orginal in_features
+    #nn.ReLU(), #ReLu to be the activation function
+    #nn.Dropout(p=0.3),
+    #nn.Linear(in_features=625, out_features=256),
+    #nn.ReLU(),
+    #nn.Linear(in_features=256, out_features=2), 
+)
+
+#model
+
+# after updatingnow it becomes:
+#(classifier): Sequential(
+#    (0): Linear(in_features=1792, out_features=625, bias=True)
+#    (1): ReLU()
+#    (2): Dropout(p=0.3, inplace=False)
+#    (3): Linear(in_features=625, out_features=256, bias=True)
+#    (4): ReLU()
+#    (5): Linear(in_features=256, out_features=2, bias=True)
+#  )
+
+from torchsummary import  summary
+model.to(device) # move the model to GPU
+summary(model,input_size=(3,640,640))
+
+class PneumoniaTrainer():
+    
+    def __init__(self,criterion = None,optimizer = None,schedular = None):
+        
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.schedular = schedular
+    
+    def train_batch_loop(self,model,trainloader):
+        
+        train_loss = 0.0
+        train_acc = 0.0
+        
+        for images,labels in tqdm(trainloader): 
+            
+            # move the data to CPU
+            images = images.to(device)
+            labels = labels.to(device)
+            
+            logits = model(images)
+            loss = self.criterion(logits,labels)
+            
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+            
+            train_loss += loss.item()
+            train_acc += accuracy(logits,labels)
+            
+        return train_loss / len(trainloader), train_acc / len(trainloader) 
+
+    
+    def valid_batch_loop(self,model,validloader):
+        
+        valid_loss = 0.0
+        valid_acc = 0.0
+        
+        for images,labels in tqdm(validloader):
+            
+            # move the data to CPU
+            images = images.to(device) 
+            labels = labels.to(device)
+            
+            logits = model(images)
+            loss = self.criterion(logits,labels)
+            
+            valid_loss += loss.item()
+            valid_acc += accuracy(logits,labels)
+            
+        return valid_loss / len(validloader), valid_acc / len(validloader)
+            
+        
+    def fit(self,model,trainloader,validloader,epochs):
+        
+        valid_min_loss = np.Inf
+        valid_max_acc = -np.Inf
+        
+        for i in range(epochs):
+            
+            model.train() # this turn on dropout
+            avg_train_loss, avg_train_acc = self.train_batch_loop(model,trainloader) ###
+            
+            model.eval()  # this turns off the dropout lapyer and batch norm
+            avg_valid_loss, avg_valid_acc = self.valid_batch_loop(model,validloader) ###
+            
+            '''if avg_valid_loss <= valid_min_loss :
+                print("Valid_loss decreased {} --> {}".format(valid_min_loss,avg_valid_loss))
+                torch.save(model.state_dict(),'ColabDDSMModel.pt')
+                valid_min_loss = avg_valid_loss'''
+            if avg_valid_acc > valid_max_acc:
+                print("Epoch : {} Valid Acc : {:.6f} > Valid Max Acc : {:.6f}".format(i+1, avg_valid_acc, valid_max_acc))
+                torch.save(model.state_dict(),'/ddsmDataset/classification_BM_DDSM_crop/ColabDDSMModel_bestAcc_BM.pt')
+                valid_max_acc = avg_valid_acc
+
+                
+            print("Epoch : {} Train Loss : {:.6f} Train Acc : {:.6f}".format(i+1, avg_train_loss, avg_train_acc))
+            print("Epoch : {} Valid Loss : {:.6f} Valid Acc : {:.6f}".format(i+1, avg_valid_loss, avg_valid_acc))
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(),lr = CFG.lr)
+
+trainer = PneumoniaTrainer(criterion,optimizer)
+trainer.fit(model,trainloader,validationloader,epochs = CFG.epochs)
+
+model.load_state_dict(torch.load('/ddsmDataset/classification_BM_DDSM_crop/ColabDDSMModel_bestAcc_BM.pt'))
+model.eval()
+
+avg_test_loss, avg_test_acc = trainer.valid_batch_loop(model,testloader)
+
+
+print("Test Loss : {}".format(avg_test_loss))
+print("Test Acc : {}".format(avg_test_acc))
---
+++ efficientNet/efficientNetClassificationVal.py
@@ -0,0 +1,353 @@
+# -*- coding: utf-8 -*-
+
+from torchvision import transforms as T, datasets
+from torchsummary import summary
+import timm  # PyTorch Image Models
+import torch.nn.functional as F
+from torch import nn
+from torchvision.utils import make_grid
+from torch.utils.data import DataLoader
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from tqdm import tqdm
+
+
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+import torch
+import numpy as np
+
+
+def show_image(image, label, get_denormalize=True):
+
+    image = image.permute(1, 2, 0)
+    mean = torch.FloatTensor([0.485, 0.456, 0.406])
+    std = torch.FloatTensor([0.229, 0.224, 0.225])
+
+    if get_denormalize == True:
+        image = image*std + mean
+        image = np.clip(image, 0, 1)
+        plt.imshow(image)
+        plt.title(label)
+
+    else:
+        plt.imshow(image)
+        plt.title(label)
+
+
+def show_grid(image, title=None):
+
+    image = image.permute(1, 2, 0)
+    mean = torch.FloatTensor([0.485, 0.456, 0.406])
+    std = torch.FloatTensor([0.229, 0.224, 0.225])
+
+    image = image*std + mean
+    image = np.clip(image, 0, 1)
+
+    plt.figure(figsize=[15, 15])
+    plt.imshow(image)
+    if title != None:
+        plt.title(title)
+
+
+def accuracy(y_pred, y_true):
+    y_pred = F.softmax(y_pred, dim=1)
+    top_p, top_class = y_pred.topk(1, dim=1)
+    equals = top_class == y_true.view(*top_class.shape)
+    return torch.mean(equals.type(torch.FloatTensor))
+
+
+def view_classify(image, ps, label):
+
+    class_name = ['BENIGN', 'MALIGNANT']
+    classes = np.array(class_name)
+
+    ps = ps.cpu().data.numpy().squeeze()
+
+    image = image.permute(1, 2, 0)
+    mean = torch.FloatTensor([0.485, 0.456, 0.406])
+    std = torch.FloatTensor([0.229, 0.224, 0.225])
+
+    image = image*std + mean
+    img = np.clip(image, 0, 1)
+
+    fig, (ax1, ax2) = plt.subplots(figsize=(8, 12), ncols=2)
+    ax1.imshow(img)
+    ax1.set_title('Ground Truth : {}'.format(class_name[label]))
+    ax1.axis('off')
+    ax2.barh(classes, ps)
+    ax2.set_aspect(0.1)
+    ax2.set_yticks(classes)
+    ax2.set_yticklabels(classes)
+    ax2.set_title('Predicted Class')
+    ax2.set_xlim(0, 1.1)
+
+    plt.tight_layout()
+
+    return None
+
+
+class CFG:
+
+    epochs = 50                              # No. of epochs for training the model
+    lr = 0.001                              # Learning rate
+    batch_size = 16                         # Batch Size for Dataset
+
+    # Model name (we are going to import model from timm)
+    model_name = 'tf_efficientnet_b4_ns'
+    img_size = 640                          # Resize all the images to be 224 by 224
+
+    # going to be used for loading dataset
+    #Data_DIR = "chest_xray"
+    #TEST = "test"
+    #TRAIN = "train"
+    #VAL = "val"
+    train_path = '/ddsmDataset/classification4/train'
+    validate_path = '/ddsmDataset/classification4/val'
+    test_path = '/home/islab/vincentwang/project/proc/code/cropResult'
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("On which device we are on:{}".format(device))
+
+
+train_transform = T.Compose([
+
+    # Resizing the image to be 224 by 224
+    T.Resize(size=(CFG.img_size, CFG.img_size)),
+    # Randomly Rotate Images by +/- 20 degrees, Image argumentation for each epoch
+    T.RandomRotation(degrees=(-90, +90)),
+    T.RandomHorizontalFlip(0.5),
+    T.RandomVerticalFlip(0.5),
+    T.ToTensor(),  # converting the dimension from (height,weight,channel) to (channel,height,weight) convention of PyTorch
+    # Normalize by 3 means 3 StD's of the image net, 3 channels
+    T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+
+])
+
+validate_transform = T.Compose([
+
+    # Resizing the image to be 224 by 224
+    T.Resize(size=(CFG.img_size, CFG.img_size)),
+    # T.RandomRotation(degrees=(-20,+20)), #NO need for validation
+    T.ToTensor(),  # converting the dimension from (height,weight,channel) to (channel,height,weight) convention of PyTorch
+    # Normalize by 3 means 3 StD's of the image net, 3 channels
+    T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+
+])
+
+test_transform = T.Compose([
+
+    # Resizing the image to be 224 by 224
+    T.Resize(size=(CFG.img_size, CFG.img_size)),
+    # T.RandomRotation(degrees=(-20,+20)), #NO need for validation
+    T.ToTensor(),  # converting the dimension from (height,weight,channel) to (channel,height,weight) convention of PyTorch
+    # Normalize by 3 means 3 StD's of the image net, 3 channels
+    T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+
+])
+
+trainset = datasets.ImageFolder(CFG.train_path, transform=train_transform)
+print("Trainset Size:  {}".format(len(trainset)))
+
+validateset = datasets.ImageFolder(
+    CFG.validate_path, transform=validate_transform)
+print("validateset Size:  {}".format(len(validateset)))
+
+testset = datasets.ImageFolder(CFG.test_path, transform=test_transform)
+print("testset Size:  {}".format(len(testset)))
+
+img, label = trainset[20]
+# print(trainset.class_to_idx)
+
+class_name = ['BENIGN', 'MALIGNANT']
+# show_image(img,class_name[label])
+
+img, label = trainset[20]
+# print(trainset.class_to_idx)
+
+class_name = ['BENIGN', 'MALIGNANT']
+# show_image(img,class_name[label])
+
+# randomly rotated
+
+
+trainloader = DataLoader(trainset, batch_size=CFG.batch_size, shuffle=True)
+# Trainset Size:  5216 / batch_size: 16 = 326(No. of batches in trainloader)
+print("No. of batches in trainloader:{}".format(len(trainloader)))
+print("No. of Total examples:{}".format(len(trainloader.dataset)))
+
+validationloader = DataLoader(
+    validateset, batch_size=CFG.batch_size, shuffle=True)
+# validationset Size:  16 / batch_size: 16 = 1(No. of batches in validationloader)
+print("No. of batches in validationloader:{}".format(len(validationloader)))
+print("No. of Total examples:{}".format(len(validationloader.dataset)))
+
+testloader = DataLoader(testset, batch_size=CFG.batch_size, shuffle=True)
+# testset Size:  624 / batch_size: 16 = 39(No. of batches in testloader)
+print("No. of batches in testloader:{}".format(len(testloader)))
+print("No. of Total examples:{}".format(len(testloader.dataset)))
+
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+
+out = make_grid(images, nrow=4)
+
+#show_grid(out,title = [class_name[x] for x in labels])
+
+
+# load pretrained model
+model = timm.create_model(CFG.model_name, pretrained=True)
+
+# model
+
+# let's update the pretarined model:
+for param in model.parameters():
+    param.requires_grad = False
+
+# orginally, it was:
+#(classifier): Linear(in_features=1792, out_features=1000, bias=True)
+
+
+# we are updating it as a 2-class classifier:
+model.classifier = nn.Sequential(
+    # 1792 is the orginal in_features
+    nn.Linear(in_features=1792, out_features=625),
+    nn.ReLU(),  # ReLu to be the activation function
+    nn.Dropout(p=0.3),
+    nn.Linear(in_features=625, out_features=256),
+    nn.ReLU(),
+    nn.Linear(in_features=256, out_features=2),
+)
+
+# model
+
+# after updatingnow it becomes:
+# (classifier): Sequential(
+#    (0): Linear(in_features=1792, out_features=625, bias=True)
+#    (1): ReLU()
+#    (2): Dropout(p=0.3, inplace=False)
+#    (3): Linear(in_features=625, out_features=256, bias=True)
+#    (4): ReLU()
+#    (5): Linear(in_features=256, out_features=2, bias=True)
+#  )
+
+model.to(device)  # move the model to GPU
+summary(model, input_size=(3, 640, 640))
+
+
+class PneumoniaTrainer():
+
+    def __init__(self, criterion=None, optimizer=None, schedular=None):
+
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.schedular = schedular
+
+    def train_batch_loop(self, model, trainloader):
+
+        train_loss = 0.0
+        train_acc = 0.0
+
+        for images, labels in tqdm(trainloader):
+
+            # move the data to CPU
+            images = images.to(device)
+            labels = labels.to(device)
+
+            logits = model(images)
+            loss = self.criterion(logits, labels)
+
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+            train_loss += loss.item()
+            train_acc += accuracy(logits, labels)
+
+        return train_loss / len(trainloader), train_acc / len(trainloader)
+
+    def valid_batch_loop(self, model, validloader):
+
+        valid_loss = 0.0
+        valid_acc = 0.0
+
+        for images, labels in tqdm(validloader):
+
+            # move the data to CPU
+            images = images.to(device)
+            labels = labels.to(device)
+
+            logits = model(images)
+            loss = self.criterion(logits, labels)
+
+            valid_loss += loss.item()
+            valid_acc += accuracy(logits, labels)
+
+        return valid_loss / len(validloader), valid_acc / len(validloader)
+
+    def fit(self, model, trainloader, validloader, epochs):
+
+        valid_min_loss = np.Inf
+        valid_max_acc = -np.Inf
+
+        for i in range(epochs):
+
+            model.train()  # this turn on dropout
+            avg_train_loss, avg_train_acc = self.train_batch_loop(
+                model, trainloader)
+
+            model.eval()  # this turns off the dropout lapyer and batch norm
+            avg_valid_loss, avg_valid_acc = self.valid_batch_loop(
+                model, validloader)
+
+            '''if avg_valid_loss <= valid_min_loss :
+                print("Valid_loss decreased {} --> {}".format(valid_min_loss,avg_valid_loss))
+                torch.save(model.state_dict(),'ColabDDSMModel.pt')
+                valid_min_loss = avg_valid_loss'''
+            if avg_valid_acc > valid_max_acc:
+                print("Epoch : {} Valid Acc : {:.6f} > Valid Max Acc : {:.6f}".format(
+                    i+1, avg_valid_acc, valid_max_acc))
+                torch.save(
+                    model.state_dict(), '/ddsmDataset/classification4/ColabDDSMModel_bestAcc_BM.pt')
+                valid_max_acc = avg_valid_acc
+
+            print("Epoch : {} Train Loss : {:.6f} Train Acc : {:.6f}".format(
+                i+1, avg_train_loss, avg_train_acc))
+            print("Epoch : {} Valid Loss : {:.6f} Valid Acc : {:.6f}".format(
+                i+1, avg_valid_loss, avg_valid_acc))
+
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=CFG.lr)
+
+trainer = PneumoniaTrainer(criterion, optimizer)
+
+model.load_state_dict(torch.load(
+    '/ddsmDataset/classification4/ColabDDSMModel_bestAcc_BM.pt'))
+model.eval()
+
+#avg_test_loss, avg_test_acc = trainer.valid_batch_loop(model, testloader)
+
+
+#print("Test Loss : {}".format(avg_test_loss))
+#print("Test Acc : {}".format(avg_test_acc))
+
+for i, testEle in enumerate(testset):
+    image, label = testEle
+    ps = model(image.to(device).unsqueeze(0))
+    ps = F.softmax(ps, dim=1)
+    print(testset.imgs[i][0].split("/")[-1])
+    resultText = open("./result/" + testset.imgs[i][0].split("/")[-1].replace(".jpg",".txt"),'w')
+    resultConfid = ps.cpu().data.numpy().squeeze()
+    resultText.write(str(resultConfid[0]) + " " + str(resultConfid[1]) + " " + str(resultConfid[2]))
+    resultText.close()
+'''print(testset.imgs[0])
+image, label = testEle
+ps = model(image.to(device).unsqueeze(0))
+ps = F.softmax(ps, dim=1)
+print(ps.cpu().data.numpy().squeeze())'''
+#view_classify(image, ps, label)
+
